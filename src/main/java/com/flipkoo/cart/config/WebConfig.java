38520736e1d5824993678eba2f91df672e3535ca
vikram tirumala balaji <131378627+vikram2244@users.noreply.github.com>
2025-06-23 13:17:23 +0530

    updates

--- src/main/java/com/flipkoo/cart/config/WebConfig.java
+++ src/main/java/com/flipkoo/cart/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("https://flipkoocart.netlify.app")
+                .allowedOriginPatterns("https://flipkoocart.netlify.app", "http://localhost:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
